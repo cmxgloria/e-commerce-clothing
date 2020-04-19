b4e6ffb85561d174a80d3ac03be011fe7ae26edf
Gloria Chen <cmxgloria@gmail.com>
2020-04-19 20:40:55 +1000

    Updates

---
+++ precache-manifest.b2beb04e1aa81795570653f5a5f6ba0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076a0a89e2e3a5dd9042e321a70bd593",
+    "url": "/e-commerce-clothing/index.html"
+  },
+  {
+    "revision": "89047c6a06b6c6c43f86",
+    "url": "/e-commerce-clothing/static/css/main.66954294.chunk.css"
+  },
+  {
+    "revision": "fe574bf29356dd793cec",
+    "url": "/e-commerce-clothing/static/js/2.9c4a8b2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/e-commerce-clothing/static/js/2.9c4a8b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89047c6a06b6c6c43f86",
+    "url": "/e-commerce-clothing/static/js/main.6a9f9933.chunk.js"
+  },
+  {
+    "revision": "e7d402e8f77a32e9474b",
+    "url": "/e-commerce-clothing/static/js/runtime-main.5467502c.js"
+  }
+]);
