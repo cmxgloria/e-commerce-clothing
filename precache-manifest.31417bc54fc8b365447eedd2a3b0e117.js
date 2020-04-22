2999b4e12e0454cc17ce86ba98a4be0d78705d22
Gloria Chen <cmxgloria@gmail.com>
2020-04-22 16:10:38 +1000

    Updates

---
+++ precache-manifest.31417bc54fc8b365447eedd2a3b0e117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d6499b6a2622260c4ecbbdf6dfebcf",
+    "url": "/e-commerce-clothing/index.html"
+  },
+  {
+    "revision": "e5cd9ff1a693e7e79c8f",
+    "url": "/e-commerce-clothing/static/css/main.d7cd3f05.chunk.css"
+  },
+  {
+    "revision": "f63066e9fcfe561cd9ef",
+    "url": "/e-commerce-clothing/static/js/2.32788262.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/e-commerce-clothing/static/js/2.32788262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5cd9ff1a693e7e79c8f",
+    "url": "/e-commerce-clothing/static/js/main.b4826f52.chunk.js"
+  },
+  {
+    "revision": "e7d402e8f77a32e9474b",
+    "url": "/e-commerce-clothing/static/js/runtime-main.5467502c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/e-commerce-clothing/precache-manifest.56937ac58d229cee72beff7e0be7eb23.js"
+  "/e-commerce-clothing/precache-manifest.31417bc54fc8b365447eedd2a3b0e117.js"
 );
 
 self.addEventListener('message', (event) => {
