ec8f5a4c453837e4127e6963ff2ac9d0e68a5f87
Gloria Chen <cmxgloria@gmail.com>
2020-04-24 15:02:07 +1000

    updated app.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
               ...snapShot.data(),
             },
           });
-          console.log(this.state);
+          // console.log(this.state);
         });
       }
       this.setState({
