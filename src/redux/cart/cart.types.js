8c89efd0d25cc520e7778773c97c1dc1a46b9112
gloria <cmxgloria@gmail.com>
2020-05-11 17:54:03 +1000

    created removed function using redux

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,7 @@
 const CartActionTypes = {
   TOOGLE_CART_HIDDEN: "TOOGLE_CART_HIDDEN",
   ADD_ITEM: "ADD_ITEM",
+  CLEAR_ITEM_FROM_CART: "CLEAR_ITEM_FROM_CART",
 };
 
 export default CartActionTypes;
