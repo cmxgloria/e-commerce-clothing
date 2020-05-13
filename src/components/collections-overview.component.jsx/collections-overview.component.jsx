347c144156d7ddf22e4d77dcb4f7d1be334012c7
gloria <cmxgloria@gmail.com>
2020-05-13 16:43:48 +1000

    updated css

--- src/components/collections-overview.component.jsx/collections-overview.component.jsx
+++ src/components/collections-overview.component.jsx/collections-overview.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { CollectionPreview } from "../collection-preview/collection-preview.component";
+import CollectionPreview from "../collection-preview/collection-preview.component";
 import { selectCollections } from "../../redux/shop/shop.selectors";
 import "./collections-overview.styles.scss";
 
