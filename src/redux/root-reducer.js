9acfb7fc6ebfb844565fc685c99a37f6982ab863
gloria <cmxgloria@gmail.com>
2020-05-13 14:14:45 +1000

    created directory.reducer and selectors.js and import to directory.comp

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
 const persistConfig = {
   key: "root",
@@ -14,5 +15,6 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 export default persistReducer(persistConfig, rootReducer);
