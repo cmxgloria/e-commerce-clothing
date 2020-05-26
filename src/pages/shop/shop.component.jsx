f2b5de2c9ae8fe5bbf404bde39468cf88986cabe
gloria <cmxgloria@gmail.com>
2020-05-26 12:10:57 +1000

    update shop.comp.jsx and added compoDidMount to try to bring data from database to app

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import ColletctionOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
+import { firestore } from "../../firebase/firebase.utils";
 // import { createStructuredSelector } from "reselect";
 
 // import { connect } from "react-redux";
@@ -10,15 +11,31 @@ import CollectionPage from "../collection/collection.component";
 // import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 // import { selectCollections } from "../../redux/shop/shop.selectors";
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    {/* {collections.map(({ id, ...otherCollectionProps }) => (
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection("collections");
+    collectionRef.onSnapshot(async (snapshot) => {
+      console.log(snapshot);
+    });
+  }
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        {/* {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))} */}
-    <Route exact path={`${match.path}`} component={ColletctionOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+        <Route exact path={`${match.path}`} component={ColletctionOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
 
 // move to collectionsOverview component
 // const mapStateToProps = createStructuredSelector({
